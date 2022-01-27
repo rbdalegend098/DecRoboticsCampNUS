05915c0294f2020468456c0e319f91792214f1fb
amyyliuy <97140422+amyyliuy@users.noreply.github.com>
2022-01-27 20:14:12 +0800

    Update and rename FinalProjectShuttleBus to FinalProjectShuttleBus.ino

---
+++ FinalProjectShuttleBus.ino
@@ -0,0 +1,9 @@
+/* (A) Automated Shuttle bus service robot:
+
+Turn your robot into an automated shuttle bus service. (Think MRT/MTR/LRT but without the rail track).
+It will stop at each "Station" designated for the selected "Bus" services you pick. & it will wait for the "Passengers" to descend or embark onto the shuttle bus.
+
+Hint: You can utilize the color sensor & line sensor to signify a "Bus Station".
+
+*/
+
